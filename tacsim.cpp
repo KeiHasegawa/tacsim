3891805bab263b206df5b56ee4d422b6d5fd2cfc
unknown <kei.hasegawa.18@gmail.com>
2018-09-07 23:44:54 +0100

    fix indent

--- tacsim.cpp
+++ tacsim.cpp
@@ -15,65 +15,65 @@
 extern "C" DLL_EXPORT int generator_seed()
 {
 #ifdef _MSC_VER
-        int r = _MSC_VER;
+  int r = _MSC_VER;
 #ifndef CXX_GENERATOR
-        r += 10000000;
+  r += 10000000;
 #else // CXX_GENERATOR
-        r += 20000000;
+  r += 20000000;
 #endif // CXX_GENERATOR
 #ifdef WIN32
-        r += 100000;
+  r += 100000;
 #endif // WIN32
 #endif // _MSC_VER
 #ifdef __GNUC__
-        int r = (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__);
+  int r = (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__);
 #ifndef CXX_GENERATOR
-        r += 30000000;
+  r += 30000000;
 #else // CXX_GENERATOR
-        r += 40000000;
+  r += 40000000;
 #endif // CXX_GENERATOR
 #endif // __GNUC__
-        return r;
+  return r;
 }
 
 namespace tacsim {
-        using namespace std;
-        string m_generator;
-        const char** option_handler(const char** option, int* error);
-        const char** arg_option(const char** option, int* error);
-        vector<char*> args;
-        bool setup();
+  using namespace std;
+  string m_generator;
+  const char** option_handler(const char** option, int* error);
+  const char** arg_option(const char** option, int* error);
+  vector<char*> args;
+  bool setup();
 }
 
 extern "C" DLL_EXPORT void generator_option(int argc, const char** argv, int* error)
 {
-        using namespace std;
-        using namespace tacsim;
-        m_generator = *argv;
-        ++argv;
-        --argc;
-        const char** end = argv + argc;
-        while (argv != end)
-                argv = option_handler(argv, error++);
+  using namespace std;
+  using namespace tacsim;
+  m_generator = *argv;
+  ++argv;
+  --argc;
+  const char** end = argv + argc;
+  while (argv != end)
+    argv = option_handler(argv, error++);
 }
 
 const char** tacsim::option_handler(const char** option, int* error)
 {
-        using namespace std;
-        if (string("--version") == *option) {
-                if (!m_generator.empty())
-                        cerr << m_generator << " : ";
-                cerr << "version " << "1.0" << '\n';
-                return ++option;
-        }
-        if (string("--arg") == *option)
-                return arg_option(option, error);
+  using namespace std;
+  if (string("--version") == *option) {
+    if (!m_generator.empty())
+      cerr << m_generator << " : ";
+    cerr << "version " << "1.0" << '\n';
+    return ++option;
+  }
+  if (string("--arg") == *option)
+    return arg_option(option, error);
         
-        if (!m_generator.empty())
-                cerr << m_generator << " : ";
-        cerr << "unknown option " << *option << '\n';
-        *error = 1;
-        return ++option;
+  if (!m_generator.empty())
+    cerr << m_generator << " : ";
+  cerr << "unknown option " << *option << '\n';
+  *error = 1;
+  return ++option;
 }
 
 #ifdef _MSC_VER
@@ -82,25 +82,25 @@ const char** tacsim::option_handler(const char** option, int* error)
 
 const char** tacsim::arg_option(const char** option, int* error)
 {
-        using namespace std;
+  using namespace std;
 
-        string left = "[";
-        if (!*++option || *option != left) {
-                cerr << m_generator << " : --arg option requires '['" << '\n';
-                *error = 1;
-                return ++option;
-        }
+  string left = "[";
+  if (!*++option || *option != left) {
+    cerr << m_generator << " : --arg option requires '['" << '\n';
+    *error = 1;
+    return ++option;
+  }
 
-        string right = "]";
-        while (*++option) {
-                if (right == *option)
-                        return ++option;
-                args.push_back(strdup(*option));
-        }
+  string right = "]";
+  while (*++option) {
+    if (right == *option)
+      return ++option;
+    args.push_back(strdup(*option));
+  }
 
-        cerr << m_generator << " : --arg option requires ']'" << '\n';
-        *error = 1;
-        return option;
+  cerr << m_generator << " : --arg option requires ']'" << '\n';
+  *error = 1;
+  return option;
 }
 
 
